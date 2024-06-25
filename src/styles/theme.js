4d716b5edd306ec7796330d3af1fcae9fd71287c
Jose Alarcon <joseadp5@gmail.com>
2024-06-24 23:49:36 -0400

    Primera confirmación

---
+++ src/styles/theme.js
@@ -0,0 +1,19 @@
+// src/styles/theme.js
+const theme = {
+  colors: {
+    background: '#0D1B1E',
+    text: '#000000',
+    primary: '#A44B44',
+    secondary: '#6c757d',
+    light: '#F5EBD0',
+    dark: '#0D1B1E',
+    white: '#fff'
+  },
+  breakpoints: {
+    mobile: '576px',
+    tablet: '768px',
+    desktop: '992px'
+  }
+};
+
+export default theme;
